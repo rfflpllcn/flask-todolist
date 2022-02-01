4fe84d29524bc9144522ee30ac2979e98d56f71d
rfflpllcn <raffaele.pellicani@gmail.com>
2022-02-01 19:13:29 +0100

    Required --> DataRequired

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,24 +1,24 @@
 from flask_wtf import FlaskForm
 from wtforms import PasswordField, StringField, SubmitField, ValidationError
-from wtforms.validators import Email, EqualTo, Length, Regexp, Required
+from wtforms.validators import Email, EqualTo, Length, Regexp, DataRequired
 
 from app.models import User
 
 
 class LoginForm(FlaskForm):
     email_or_username = StringField(
-        "Email or Username", validators=[Required(), Length(1, 64)]
+        "Email or Username", validators=[DataRequired(), Length(1, 64)]
     )
-    password = PasswordField("Password", validators=[Required()])
+    password = PasswordField("Password", validators=[DataRequired()])
     submit = SubmitField("Log In")
 
 
 class RegistrationForm(FlaskForm):
-    email = StringField("Email", validators=[Required(), Length(1, 64), Email()])
+    email = StringField("Email", validators=[DataRequired(), Length(1, 64), Email()])
     username = StringField(
         "Username",
         validators=[
-            Required(),
+            DataRequired(),
             Length(1, 64),
             Regexp(
                 "^[A-Za-z][A-Za-z0-9_.]*$",
@@ -30,11 +30,11 @@ class RegistrationForm(FlaskForm):
     password = PasswordField(
         "Password",
         validators=[
-            Required(),
+            DataRequired(),
             EqualTo("password_confirmation", message="Passwords must match."),
         ],
     )
-    password_confirmation = PasswordField("Confirm password", validators=[Required()])
+    password_confirmation = PasswordField("Confirm password", validators=[DataRequired()])
     submit = SubmitField("Register")
 
     def validate_email(self, field):
